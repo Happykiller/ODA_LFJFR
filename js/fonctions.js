f373c9f545b62b282f6080d4bb0dc8b4d819188a
Happykiller <fabrice.rosito@gmail.com>
2015-03-24 11:14:05 +0100

    refacto

--- js/fonctions.js
+++ js/fonctions.js
@@ -1,93 +1,67 @@
-///////////////////
-//BLOCK FONCTIONS METIER
-///////////////////
-//function init_commun()
-//function show_commun()
-//function getUserCode() 
-
-///////////////////
-//BLOCK FONCTIONS PRESENTATION
-///////////////////
-
-///////////////////
-//BLOCK FONCTIONS TECHNIQUES
-///////////////////
-//function checkAuth()
-
-///////////////////
-//BLOCK FONCTIONS AJAX JS/PHP/MYSQL
-///////////////////
-//function getAuth(p_cle) 
-
-
-//---------------------------------------------------------
-
-///////////////////
-//BLOCK Variable globale
-///////////////////
-
-///////////////////
-//BLOCK FONCTIONS METIER
-///////////////////
-function getUserCode() {
-    try {
-        return get_cookie("user");
-    } catch (er) {
-        log(0, "ERROR(getUserCode):" + er.message);
-        return "";
-    }
-}
-
-///////////////////
-//BLOCK FONCTIONS PRESENTATION
-///////////////////
-
-///////////////////
-//BLOCK FONCTIONS TECHNIQUES
-///////////////////
-function logLfjfr() {
-    try {
-        var get_cle = "";
-        get_cle = getParamGet("cle");
-        
-        if(get_cle != null){
-            var retourLog = getAuthLfjfr(get_cle);
-            var retourLevel = parseInt(retourLog["profile"]);
-            if (retourLevel > -1) {
-                set_cookie("auth",retourLog["code_user"].toUpperCase());
-                set_cookie("key",retourLog["keyAuthODA"]);
-                addStat(retourLog["code_user"],"index.html","checkAuth : ok");
-                window.location = "./page_home.html?mili="+getMilise();
-            } else {
-                addStat(retourLog["code_user"],"index.html","checkAuth : ko("+get_cle+")");
-                window.location = "page_error.html?msg=Clé incorrecte&mili="+getMilise();
-            }
-        } else{
-            addStat("INC","index.html","checkAuth : ko("+get_cle+")");
-            window.location = "page_error.html?msg=Clé absente&mili="+getMilise();
-        } 
-    }
-    catch (er) {
-        log(0, "ERROR(logLfjfr):" + er.message);
-    }
-}
-
-///////////////////
-//BLOCK FONCTIONS AJAX JS/PHP/MYSQL
-///////////////////
+// Library of tools for the exemple
 
 /**
- * @name getAuthLfjfr
- * @param {string} p_cle
- * @returns {Array}
+ * @author FRO
+ * @date 14/07/11
  */
-function getAuthLfjfr(p_cle) {
-    try {
-        var tabInput = { cle : p_cle };
-        var retour_json = callBD("phpsql/mysql_getAuth.php", "POST", "json", tabInput);
-        var data = retour_json["data"]["resultat"];
-        return data;
-    } catch (er) {
-        log(0, "ERROR(getAuthLfjfr):" + er.message);
+
+(function() {
+    'use strict';
+
+    var
+        /* version */
+        VERSION = '0.1',
+
+        _var = {
+            'hello' : 12
+        };
+
+
+    ////////////////////////// PRIVATE METHODS ////////////////////////
+
+	/**
+	 * @name _init
+	 * @desc Initialize
+	 */
+    function _init() {
+        //
     }
-}
+
+    ////////////////////////// PUBLIC METHODS /////////////////////////
+
+    $.functionsApp = {
+        /* Version number */
+        version: VERSION,
+
+        logLfjfr : function() {
+            try {
+                var get_cle = "";
+                get_cle = $.functionsLib.getParamGet("cle");
+
+                var tabInput = { cle : get_cle };
+                var retour_json = $.functionsLib.callRest("phpsql/getLogin.php", {}, tabInput);
+                if(!$.functionsLib.isUndefined(retour_json.data.login)){
+                    var retour_login = $.functionsLib.login(retour_json.data.login, get_cle);
+                    if(retour_login.statut == "ko"){
+                        $.functionsLib.log(0, "ERROR(logLfjfr): ko, wrong auth by key ("+get_cle+")");
+                        $.functionsLib.currentWindow.location = "api_page_error.html?msg=Wrong Key&mili="+$.functionsLib.getMilise();
+                        return false;
+                    }else{
+                        $.functionsLib.currentWindow.location = "page_home.html?mili="+$.functionsLib.getMilise();
+                        return true;
+                    }
+                }else{
+                    $.functionsLib.log(0, "ERROR(logLfjfr): ko, user unknown with key ("+get_cle+")");
+                    $.functionsLib.currentWindow.location = "api_page_error.html?msg=Wrong Key&mili="+$.functionsLib.getMilise();
+                    return false;
+                }
+            } catch (er) {
+                $.functionsLib.log(0, "ERROR(logLfjfr):" + er.message);
+            }
+        }
+    };
+
+    // Initialize
+    _init();
+
+})();
--- phpsql/getLogin.php
+++ phpsql/getLogin.php
@@ -13,11 +13,11 @@ $params->arrayInput = array("cle");
 $INTERFACE = new LfjfrInterface($params);
 
 //--------------------------------------------------------------------------
-// phpsql/getAuth.php?milis=123456789&cle=fuel
+// phpsql/getLogin.php?milis=123456789&cle=fuel
 
 //--------------------------------------------------------------------------
 $params = new OdaPrepareReqSql();
-$params->sql = "select a.`profile`, a.`code_user` 
+$params->sql = "select a.`login` 
     from `api_tab_utilisateurs` a
     where 1=1 
     and a.`password` = :password
@@ -28,13 +28,6 @@ $params->bindsValue = [
 $params->typeSQL = OdaLibBd::SQL_GET_ONE;
 $retour = $INTERFACE->BD_ENGINE->reqODASQL($params);
 
-if(isset($retour->data->code_user)){
-    $key = $INTERFACE->buildSession(array('code_user' => $retour->data->code_user, 'password' => $INTERFACE->inputs["cle"]));
-    
-    $params = new stdClass();
-    $params->label = "keyAuthODA";
-    $params->value = $key;
-    $INTERFACE->addDataStr($params);
-}else{
-    $INTERFACE->dieInError("key unknown.");
-}
+$params = new stdClass();
+$params->retourSql = $retour;
+$INTERFACE->addDataReqSQL($params);
