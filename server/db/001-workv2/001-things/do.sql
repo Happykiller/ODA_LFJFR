630d106c20ba63cd8d56bd97b8a5d1947a27cb3e
happykiller <fabrice.rosito@gmail.com>
2015-10-23 16:32:53 +0200

    work v2

---
+++ server/db/001-workv2/001-things/do.sql
@@ -0,0 +1,5 @@
+UPDATE `@prefix@api_tab_menu_categorie` SET `Description` = 'CV' WHERE `api_tab_menu_categorie`.`id` = 3;
+UPDATE `@prefix@api_tab_menu` SET `id_categorie` = '1' WHERE `api_tab_menu`.`id` = 3;
+UPDATE `@prefix@api_tab_menu` SET `id_categorie` = '3' WHERE `api_tab_menu`.`id` = 70;
+UPDATE `@prefix@api_tab_menu` SET `id_categorie` = '3' WHERE `api_tab_menu`.`id` = 72;
+UPDATE `@prefix@api_tab_menu` SET `id_categorie` = '3' WHERE `api_tab_menu`.`id` = 73;
