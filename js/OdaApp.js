67d56068235043e1825e5c2b38057f49ca394560
Rosito Fabrice <fabrice.rosito@gmail.com>
2015-10-25 18:44:47 +0100

    work v2

--- js/OdaApp.js
+++ js/OdaApp.js
@@ -104,12 +104,27 @@
                 }
             },
             Passe: {
+                lastFilter : '',
                 /**
                  * @returns {$.Oda.App.Controler.Passe}
                  */
                 start: function () {
                     try {
                         $.Oda.App.Controler.Passe.loadExp({"filter":""});
+
+                        $.Oda.Scope.Gardian.add({
+                            id : "gardianFilter",
+                            listElt : ["filter"],
+                            function : function(params){
+                                var filter = $('#'+params.elt).val();
+                                if($.Oda.App.Controler.Passe.lastFilter !== filter){
+                                    $.Oda.App.Controler.Passe.lastFilter = filter;
+                                    $.Oda.Tooling.debounce(function(){
+                                        $.Oda.App.Controler.Passe.loadExp({"filter":filter});
+                                    },500);
+                                }
+                            }
+                        });
                         return this;
                     } catch (er) {
                         $.Oda.Log.error("$.Oda.App.Controler.Passe.start : " + er.message);
@@ -125,28 +140,84 @@
                     try {
                         var tabInput = { "critere" : p_params.filter };
                         var call = $.Oda.Interface.callRest($.Oda.Context.rest+"phpsql/getExperiences.php", {"functionRetour": function(response){
-                            $.each( response.data.resultat.data, function( index, value ) {
-                                var strHtml = $.Oda.Display.TemplateHtml.create({
-                                    "template" : "experience"
-                                    , "scope" : {
-                                        "title" : value.titre_experience,
-                                        "cmt" :  value.commentaire_titre,
-                                        "customer" : value.employeur,
-                                        "begin" : value.debut,
-                                        "end" : value.fin,
-                                        "fonction" : value.fonction,
-                                        "secteur" : value.secteur,
-                                        "fonctionnel" : value.fonctionnel,
-                                        "realisation" : value.realisation,
-                                        "language" : value.language,
-                                        "applicatif" : value.applicatif,
-                                        "develloppement" : value.develloppement,
-                                        "bdd" : value.bdd
+                            if(response.data.resultat.nombre === 0){
+                                $.Oda.Display.Notification.warning('Pas de r&eacute;sultat pour : '+p_params.filter);
+                            }else{
+                                $('#listExp').html('');
+                                $.Oda.Display.Notification.info('Nombre de r&eacute;sultat trouv&eacute; : '+response.data.resultat.nombre);
+                                $.each( response.data.resultat.data, function( index, value ) {
+                                    var cmt = "";
+                                    if(value.commentaire_titre !== ""){
+                                        cmt = '<h5>'+ $.Oda.Tooling.replaceAll({"str":value.commentaire_titre, "find":p_params.filter, "by":'<span style="color: red">'+p_params.filter+'</span>', "ignoreCase":true})+'</h5><hr>';
                                     }
-                                });
 
-                                $('#listExp').after(strHtml);
-                            });
+                                    var role = "";
+                                    if(value.fonction !== ""){
+                                        role = '<dt><b>Fonction</b></dt><dd>'+$.Oda.Tooling.replaceAll({"str":value.fonction, "find":p_params.filter, "by":'<span style="color: red">'+p_params.filter+'</span>', "ignoreCase":true})+'</dd>';
+                                    }
+
+                                    var secteur = "";
+                                    if(value.secteur !== ""){
+                                        secteur = '<dt><b>Secteur</b></dt><dd>'+$.Oda.Tooling.replaceAll({"str":value.secteur, "find":p_params.filter, "by":'<span style="color: red">'+p_params.filter+'</span>', "ignoreCase":true})+'</dd>';
+                                    }
+
+                                    var fonctionnel = "";
+                                    if(value.fonctionnel !== ""){
+                                        fonctionnel = '<dt><b>Conception fonctionnelle</b></dt><dd>'+$.Oda.Tooling.replaceAll({"str":value.fonctionnel, "find":p_params.filter, "by":'<span style="color: red">'+p_params.filter+'</span>', "ignoreCase":true})+'</dd>';
+                                    }
+
+                                    var realisation = "";
+                                    if(value.realisation !== ""){
+                                        realisation = '<dt><b>R&eacute;alisation</b></dt><dd>'+$.Oda.Tooling.replaceAll({"str":value.realisation, "find":p_params.filter, "by":'<span style="color: red">'+p_params.filter+'</span>', "ignoreCase":true})+'</dd>';
+                                    }
+
+                                    var realisation = "";
+                                    if(value.realisation !== ""){
+                                        realisation = '<dt><b>R&eacute;alisation</b></dt><dd>'+$.Oda.Tooling.replaceAll({"str":value.realisation, "find":p_params.filter, "by":'<span style="color: red">'+p_params.filter+'</span>', "ignoreCase":true})+'</dd>';
+                                    }
+
+                                    var language = "";
+                                    if(value.language !== ""){
+                                        language = '<dt><b>Language</b></dt><dd>'+$.Oda.Tooling.replaceAll({"str":value.language, "find":p_params.filter, "by":'<span style="color: red">'+p_params.filter+'</span>', "ignoreCase":true})+'</dd>';
+                                    }
+
+                                    var applicatif = "";
+                                    if(value.applicatif !== ""){
+                                        applicatif = '<dt><b>Applicatif</b></dt><dd>'+$.Oda.Tooling.replaceAll({"str":value.applicatif, "find":p_params.filter, "by":'<span style="color: red">'+p_params.filter+'</span>', "ignoreCase":true})+'</dd>';
+                                    }
+
+                                    var develloppement = "";
+                                    if(value.develloppement !== ""){
+                                        develloppement = '<dt><b>Environnement devellopement</b></dt><dd>'+$.Oda.Tooling.replaceAll({"str":value.develloppement, "find":p_params.filter, "by":'<span style="color: red">'+p_params.filter+'</span>', "ignoreCase":true})+'</dd>';
+                                    }
+
+                                    var bdd = "";
+                                    if(value.bdd !== ""){
+                                        bdd = '<dt><b>Environnement base de donn&eacute;es</b></dt><dd>'+$.Oda.Tooling.replaceAll({"str":value.bdd, "find":p_params.filter, "by":'<span style="color: red">'+p_params.filter+'</span>', "ignoreCase":true})+'</dd>';
+                                    }
+
+                                    var strHtml = $.Oda.Display.TemplateHtml.create({
+                                        "template" : "experience"
+                                        , "scope" : {
+                                            "title" : value.titre_experience,
+                                            "cmt" :  cmt,
+                                            "customer" : value.employeur,
+                                            "begin" : value.debut,
+                                            "end" : value.fin,
+                                            "fonction" : role,
+                                            "secteur" : secteur,
+                                            "fonctionnel" : fonctionnel,
+                                            "realisation" : realisation,
+                                            "language" : language,
+                                            "applicatif" : applicatif,
+                                            "develloppement" : develloppement,
+                                            "bdd" : bdd
+                                        }
+                                    });
+
+                                    $('#listExp').append(strHtml);
+                                });
+                            }
                         }}, tabInput);
 
                         return this;
